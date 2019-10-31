1b328c7d5fa2998a52a5d1def7872d3511e490d4
Eduardo Pereira Ribeiro <epribeiro@localhost.localdomain>
2019-10-31 10:23:11 -0300

    Desafio nº 4

---
+++ APIRest/src/test/java/com/pgg/registrochave/RegistroChaveTests.java
@@ -0,0 +1,13 @@
+package com.pgg.registrochave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroChaveTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
