3e19f7432c70977780d52e1571bf5da8c141989f
Eduardo Pereira Ribeiro <epribeiro@localhost.localdomain>
2019-11-01 14:00:51 -0300

    Erros dentro do comando 'list' arrumados

--- RegistroChave/src/main/java/com/pgg/registrochave/RegistroChave.java
+++ RegistroChave/src/main/java/com/pgg/registrochave/RegistroChave.java
@@ -2,6 +2,7 @@ package com.pgg.registrochave;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.util.List;
 
 @SpringBootApplication
@@ -19,26 +20,26 @@ public class RegistroChave implements CommandLineRunner {
             for (int i = 0; i < args.length; i++) entrada += args[i] + " ";
 
             String comando = entrada.split(" ")[0];
-            String chave = entrada.split("")[1];
+            String chave = "";
+            if (!comando.equals("list"))  chave = entrada.split(" ")[1];
 
-            if (!(chave.equals("") || chave.equals(" "))) {
-                switch (comando) {
-                    case "set":
-                        LeitorComando.setValor(chave);
-                        break;
-                    case "get":
-                        System.out.println(LeitorComando.getValor(chave));
-                        break;
-                    case "delete":
-                        LeitorComando.deleteValor(chave);
-                        break;
-                    case "list":
-                        List<String> resultado = LeitorComando.listValor();
-                        resultado.stream().forEach(System.out::println);
-                        break;
-                    default: System.out.println("O comando não foi encontrado...");
-                }
-            } else System.out.println("Não foi possível selecionar a chave...");
+            switch (comando) {
+                case "set":
+                    LeitorComando.setValor(chave);
+                    break;
+                case "get":
+                    System.out.println(LeitorComando.getValor(chave));
+                    break;
+                case "delete":
+                    LeitorComando.deleteValor(chave);
+                    break;
+                case "list":
+                    List<String> resultado = LeitorComando.listValor();
+                    resultado.stream().forEach(System.out::println);
+                    break;
+                default:
+                    System.out.println("O comando não foi encontrado...");
+            }
         } catch (Exception e) {
             System.out.println("Ocorreu o seguinte erro: " + e);
         }
